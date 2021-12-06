be467ca5f6a5426a16a81a3e8082749d2760fe0c
weicheng lee <z82111712@gmail.com>
2021-12-05 17:01:58 -0800

    POST route lect

--- src/main/java/com/wei/firstproject/HomeController.java
+++ src/main/java/com/wei/firstproject/HomeController.java
@@ -1,17 +1,62 @@
 package com.wei.firstproject;
 
-import java.util.ArrayList;
+import java.util.Date;
 
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 //@RestController
 //@RequestMapping("/hello")
 public class HomeController {
+	
+	// index route
+	// @GetMapping for default GET request
+	@RequestMapping("/")
+	public String index() {
+		return "index.jsp";
+	}
+	
+	// payment process post
+	// @PostMapping("/processPayment") -- shorthand  for POST
+	@RequestMapping(value="/processPayment", method=RequestMethod.POST)
+	public String processPayment(
+			@RequestParam(value="creditCardNumber") Integer creditCardNumber,
+			@RequestParam(value="exDate") Date expDate,
+			@RequestParam(value="productID") Integer productID
+			)
+	{
+		
+		System.out.println("" + creditCardNumber + expDate + productID);
+		System.out.printf("Chargin credit, timestamp: %s",new Date());
+		
+		return "redirect:/confirm";
+	}
+	
+	// payment confirm page
+	@RequestMapping("/confirm")
+	public String confirmationPage(Model model) {
+		return "confirmation.jsp";
+	}
+	
+	
+	
+	// route for login submission
+	// @PostMapping("/login") -- shorthand  for POST
+	@RequestMapping(value="/login", method=RequestMethod.POST)
+	public String login(
+			@RequestParam(value="email") String email,
+			@RequestParam(value="password")String password
+			) 
+	{
+		return "redirect:/dashboard";
+	}
+	
 	
 	public String indexSession(HttpSession session) {
 		// check if session not exist will return null
@@ -35,38 +80,37 @@ public class HomeController {
 		
 		return "showCount.jsp";
 	}
-	
-	@RequestMapping("/")
-	public String index(Model model) {
-		model.addAttribute("fruit", "banana");
-		
-		String firstName = "Ada";
-		String lastName = "Lovelace";
-		String email = "ones@zeroes.com";
-		Integer age = 160;
-		
-		model.addAttribute("firstName", firstName);
-		model.addAttribute("lastName", lastName);
-		model.addAttribute("email", email);
-		model.addAttribute("age", age);
-		
-		
-		 
-		return "index.jsp";
-		
 
-	}
-	@RequestMapping("/dojos")
-	public String indexDojo(Model model) {
-		ArrayList<String> dojos = new ArrayList<String>();
-		
-		dojos.add("Brubank");
-		dojos.add("Chicago");
-		dojos.add("Bellevue");
-		
-		model.addAttribute("dojosList", dojos);
-		return "index.jsp";
-	}
+	
+//	@RequestMapping("/")
+//	public String index(Model model) {
+//		model.addAttribute("fruit", "banana");
+//		
+//		String firstName = "Ada";
+//		String lastName = "Lovelace";
+//		String email = "ones@zeroes.com";
+//		Integer age = 160;
+//		
+//		model.addAttribute("firstName", firstName);
+//		model.addAttribute("lastName", lastName);
+//		model.addAttribute("email", email);
+//		model.addAttribute("age", age);
+// 
+//		return "index.jsp";
+//	}
+	
+
+//	@RequestMapping("/dojos")
+//	public String indexDojo(Model model) {
+//		ArrayList<String> dojos = new ArrayList<String>();
+//		
+//		dojos.add("Brubank");
+//		dojos.add("Chicago");
+//		dojos.add("Bellevue");
+//		
+//		model.addAttribute("dojosList", dojos);
+//		return "index.jsp";
+//	}
 //	// function(annotation(query field, required=false)  String input)
 //	public String index(
 //			@RequestParam(value="q", required=false) String searchQuery
@@ -79,7 +123,8 @@ public class HomeController {
 //			
 //		}
 //	}
-//	
+
+	
 //	@RequestMapping("/m/{track}/{module}/{lesson}")
 //	public String showLesson(
 //			// part of url as we set in request mapping
@@ -90,6 +135,7 @@ public class HomeController {
 //	{
 //		return track + " " + module + " " + lesson;
 //	}
+	
 	
 //	@RequestMapping("")
 //	public String hello() {
--- src/main/webapp/WEB-INF/index.jsp
+++ src/main/webapp/WEB-INF/index.jsp
@@ -6,7 +6,7 @@
 <html>
 <head>
 	<meta charset="UTF-8">
-	<title>Fruit Page</title>
+	<title>First Project</title>
 	<!-- YOUR own local CSS -->
 	<link rel="stylesheet" href="/css/style.css"/>
 	<!-- <script type="text/javascript" src="js/script.js"></script> -->
@@ -23,28 +23,46 @@
 	<h2><c:out value="${2+2}"/></h2> --%>
 	
 	<div class="container">
+		<h3>Login</h3>
+		<form action="/login" method="post">
+			<label>Email:</label>
+			<input type="email" name="email">
+			<label>Password:</label>
+			<input type="password" name="password">
+	        <input type="submit">
+		</form>
+		
+		<h3>Search</h3>
+			<form action="/search">
+	        <label>Search:</label>
+	        <input type="text" name="searchTerm">
+	        <input type="submit">
+    	</form>
+    	
+    	<h3>Pay</h3>
+    	<form action="processPayment" method="post">
+    		<input type="hidden" name="productID" value="128">
+    		<label>Credit Card Number</label>
+    		<input type="text" name="creditCardNumber">
+    		<label>Expiration Date</label>
+    		<input type="date" name="expDate">
+    		<input type="submit">
+    	</form>
+		<%-- 		
 		<h1 class="text-dark">Dojo Location</h1>
 			<!-- this will only run if dojoList pass in -->
 			<c:forEach var="dojo" items="${ dojosList }">
 				<p> <c:out value="${ dojo }"/> </p>
 			</c:forEach>
+		--%>
 		<!-- h2 will still create but if route not providing value it'll be empty tag -->
-		<h2>
-			<c:out value="${fruit}" />
-		</h2>
-	
-		<h2>
-			<c:out value="${firstName}" />
-		</h2>
-		<h2>
-			<c:out value="${lastName}" />
-		</h2>
-		<h2>
-			<c:out value="${email}" />
-		</h2>
-		<h2>
-			<c:out value="${age}" />
-		</h2>
+		<%--
+		<h2><c:out value="${fruit}" /></h2>
+		<h2><c:out value="${firstName}" /></h2>
+		<h2><c:out value="${lastName}" /></h2>
+		<h2><c:out value="${email}" /></h2>
+		<h2><c:out value="${age}" /></h2>
+		--%>
 	</div>
 
 
